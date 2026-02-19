94eb1f2eda3ee51c6698262d2e8706629c2788fe
Will-Carlo <wil__fer_25@outlook.es>
2026-02-18 23:19:27 -0400

    se ha agregado diseños de la empresa;

--- src/services/commonService.ts
+++
@@ -1,8 +0,0 @@
-import api from '../lib/axios';
-import { type City } from '../features/inventory/types/index';
-
-export const getCities = async (): Promise<City[]> => {
-    // Asumiendo que devuelve array directo según tu ejemplo, si tiene wrapper ajustamos
-    const { data } = await api.get<City[]>('/generic/cities');
-    return data;
-};
